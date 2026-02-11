63514508c5f73257eda7617c5a79239aa7656573
samarthsinh2660 <rajuvala80@gmail.com>
2026-02-11 22:29:15 +0530

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.variable} font-sans antialiased`}>
+      <body className={`${inter.variable} font-sans antialiased`} suppressHydrationWarning>
         <ApolloProvider>{children}</ApolloProvider>
       </body>
     </html>
